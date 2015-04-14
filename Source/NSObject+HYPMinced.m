a02a9e23e2d7789d0df9bdd2fab880642b49ae8a
jacob-andersson <jacke.andersson@gmail.com>
2015-04-14 17:15:22 +0200

    Extend API with possibility of replacing null values with empty string + add test

--- Source/NSObject+HYPMinced.m
+++ Source/NSObject+HYPMinced.m
@@ -4,44 +4,80 @@
 
 @implementation NSObject (HYPMinced)
 
+#pragma mark - Public
+
 - (id)minced_JSONKeysToCamelCase
 {
-    if ([self isKindOfClass:NSArray.class]) {
-        return [self minced_JSONObjectsKeysToCamelCase];
-    } else if ([self isKindOfClass:NSDictionary.class]) {
-        return [self minced_JSONObjectKeysToCamelCase];
-    }
+    return [self minced_JSONKeysToCamelCaseWithValuesUnnillified:NO];
+}
 
-    return self;
+- (id)minced_JSONKeysToCamelCaseWithValuesUnnullified
+{
+    return [self minced_JSONKeysToCamelCaseWithValuesUnnillified:YES];
 }
 
 - (NSArray *)minced_JSONObjectsKeysToCamelCase
 {
-    NSMutableArray *camelCaseJSON = [NSMutableArray new];
+    return [self minced_JSONObjectsKeysToCamelCaseWithValuesUnnullified:NO];
+}
 
-    NSArray *JSONObjects = (NSArray *)self;
-    for (NSDictionary *object in JSONObjects) {
-        [camelCaseJSON addObject:[object minced_JSONObjectKeysToCamelCase]];
-    }
-
-    return [camelCaseJSON copy];
+- (NSArray *)minced_JSONObjectsKeysToCamelCaseWithValuesUnnullified
+{
+    return [self minced_JSONObjectsKeysToCamelCaseWithValuesUnnullified:YES];
 }
 
 - (NSDictionary *)minced_JSONObjectKeysToCamelCase
 {
-    NSMutableDictionary *camelCaseJSONObject = [NSMutableDictionary new];
+    return [self minced_JSONObjectKeysToCamelCaseWithValuesUnnullified:NO];
+}
 
+- (NSDictionary *)minced_JSONObjectKeysToCamelCaseWithValuesUnnullified
+{
+    return [self minced_JSONObjectKeysToCamelCaseWithValuesUnnullified:YES];
+}
+
+#pragma mark - Private
+
+- (id)minced_JSONKeysToCamelCaseWithValuesUnnillified:(BOOL)unnullify
+{
+    if ([self isKindOfClass:NSArray.class]) {
+        return [self minced_JSONObjectsKeysToCamelCaseWithValuesUnnullified:unnullify];
+    } else if ([self isKindOfClass:NSDictionary.class]) {
+        return [self minced_JSONObjectKeysToCamelCaseWithValuesUnnullified:unnullify];
+    }
+    
+    return self;
+}
+
+- (NSArray *)minced_JSONObjectsKeysToCamelCaseWithValuesUnnullified:(BOOL)unnullify
+{
+    NSMutableArray *camelCaseJSON = [NSMutableArray new];
+    
+    NSArray *JSONObjects = (NSArray *)self;
+    for (NSDictionary *object in JSONObjects) {
+        [camelCaseJSON addObject:[object minced_JSONObjectKeysToCamelCaseWithValuesUnnullified:unnullify]];
+    }
+    
+    return [camelCaseJSON copy];
+}
+
+- (NSDictionary *)minced_JSONObjectKeysToCamelCaseWithValuesUnnullified:(BOOL)unnullify
+{
+    NSMutableDictionary *camelCaseJSONObject = [NSMutableDictionary new];
+    
     NSDictionary *JSONObject = (NSDictionary *)self;
     [JSONObject enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
         if ([obj isKindOfClass:NSArray.class]) {
-            camelCaseJSONObject[[key hyp_localString]] = [obj minced_JSONKeysToCamelCase];
+            camelCaseJSONObject[[key hyp_localString]] = [obj minced_JSONKeysToCamelCaseWithValuesUnnillified:unnullify];
         } else if ([obj isKindOfClass:NSDictionary.class]) {
-            camelCaseJSONObject[[key hyp_localString]] = [obj minced_JSONObjectKeysToCamelCase];
+            camelCaseJSONObject[[key hyp_localString]] = [obj minced_JSONObjectKeysToCamelCaseWithValuesUnnullified:unnullify];
+        } else if (unnullify) {
+            camelCaseJSONObject[[key hyp_localString]] = (obj == nil || [obj isKindOfClass:NSNull.class]) ? @"" : obj;
         } else {
             camelCaseJSONObject[[key hyp_localString]] = obj;
         }
     }];
-
+    
     return [camelCaseJSONObject copy];
 }
 
