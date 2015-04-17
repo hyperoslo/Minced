5a99210d3a3404cda8cf29caf44189fd25ecb012
Elvis Nuñez <elvisnunez@me.com>
2015-04-18 00:59:00 +0200

    Improve tests using JSON

--- Tests/Tests/Tests.m
+++ Tests/Tests/Tests.m
@@ -1,6 +1,7 @@
 @import XCTest;
 
 #import "NSObject+HYPMinced.h"
+#import "JSON.h"
 
 @interface Tests : XCTestCase
 
@@ -9,27 +10,21 @@
 @implementation Tests
 
 - (void)testJSONKeys {
-    id snakeCaseJSON = [self serializedJSON:@"snakeCaseJSON"];
-    id camelCaseJSON = [self serializedJSON:@"camelCaseJSON"];
+    id snakeCaseJSON = [JSON fromFileNamed:@"snakeCaseJSON.json"
+                                  inBundle:[NSBundle bundleForClass:[self class]]];
+    id camelCaseJSON = [JSON fromFileNamed:@"camelCaseJSON.json"
+                                  inBundle:[NSBundle bundleForClass:[self class]]];
 
     XCTAssertEqualObjects([snakeCaseJSON minced_JSONKeys], camelCaseJSON);
 }
 
 - (void)testJSONKeysWithNonnulls {
-    id nullSnakeCaseJSON = [self serializedJSON:@"nullSnakeCaseJSON"];
-    id nonnullCamelCaseJSON = [self serializedJSON:@"nonnullCamelCaseJSON"];
+    id nullSnakeCaseJSON = [JSON fromFileNamed:@"nullSnakeCaseJSON.json"
+                                      inBundle:[NSBundle bundleForClass:[self class]]];
+    id nonnullCamelCaseJSON = [JSON fromFileNamed:@"nonnullCamelCaseJSON.json"
+                                         inBundle:[NSBundle bundleForClass:[self class]]];
 
     XCTAssertEqualObjects([nullSnakeCaseJSON minced_JSONKeysWithNonnulls], nonnullCamelCaseJSON);
-}
-
-- (id)serializedJSON:(NSString *)resource {
-    NSString *JSONFilePath = [[NSBundle bundleForClass:self.class] pathForResource:resource ofType:@"json"];
-    NSData *JSONData = [[NSData alloc] initWithContentsOfFile:JSONFilePath];
-    NSError *error = nil;
-
-    return [NSJSONSerialization JSONObjectWithData:JSONData
-                                           options:kNilOptions
-                                             error:&error];
 }
 
 @end
